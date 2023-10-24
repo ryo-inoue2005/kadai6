14b1883bf5317dfd674d9e5c200db15863856c21
ryo-inoue2005 <138546467+ryo-inoue2005@users.noreply.github.com>
2023-10-24 13:24:09 +0900

    追加課題6回目修正です
    ・住所から郵便番号の検索ロジックを変更しました。(/action/ResultSharingAction.java  172行目  addressToZipcode())
    ・複数の郵便番号が取得される住所の場合、選択しても取得を繰り返してしまう不具合を修正しました。(/resultSharing/addressListSubWindowView.jsp  76行目〜96行目)

--- src/main/resources/sql/findZipcodeByFulladdress.sql
+++ src/main/resources/sql/findZipcodeByFulladdress.sql
@@ -1,4 +1,8 @@
 SELECT zip_code, prefecture, city, address
 FROM zipcode
-WHERE CONCAT(prefecture, city, address)
-LIKE /*fullAddress*/'S' || '%'
+WHERE zip_code IN (
+	SELECT zip_code
+	FROM search_zipcode 
+	WHERE CONCAT(prefecture, city, address)
+	LIKE /*fullAddress*/'S' || '%'
+)
